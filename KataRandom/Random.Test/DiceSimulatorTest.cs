9d1acfa460407620a9b30efbc781f96dc393025d
Thomas Kaelin <thomas.kaelin@bbv.ch>
2016-10-04 10:27:05 +0200

    [Random] added starter solution.

---
+++ KataRandom/Random.Test/DiceSimulatorTest.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using Random;
+
+namespace KataRandom
+{
+    /// <summary>
+    /// Probleme:
+    /// 1) Zufälliges Verhalten wg. Random
+    /// 2) Output auf Console
+    /// 3) Exception bei Spezialfall
+    /// 4) Langlaufende Berechnung (simuliert durch Thread.Sleep)
+    /// 
+    /// Testfälle:
+    /// 0) Ziel: FIRST (Fast, Independent, Repeatable, Self Validating, Timely)
+    /// 1) Verhalten bei "normalen" Werten (1-6)
+    /// 2) Verhalten bei "invalidem" Wert (0, 7)
+    /// 
+    /// Designfragen:
+    /// 1) SRP eingehalten?
+    /// 2) OCP eingehalten?
+    /// 3) DIP eingehalten?
+    /// 4) DRY eingehalten?
+    /// 5) Clean Code - Namen?
+    /// 6) Clean Code - Funktionen?
+    /// 7) Clean Code - Kommentare?
+    /// </summary>
+    [TestFixture]
+    public class DiceSimulatorTest
+    {
+        private DiceSimulator _target;
+
+        [SetUp]
+        public void Setup()
+        {
+            _target = new DiceSimulator();
+        }
+
+        [Test]
+        public void Do_ShouldDoSomething()
+        {
+            // Act
+            _target.Do();
+
+            // Assert
+            // TODO Wie das Verhalten überprüfen?
+        }
+    }
+}
---
+++ KataRandom/Random/InvalidNumberException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Random
+{
+    public sealed class InvalidNumberException : Exception
+    {
+        public InvalidNumberException() : base("An invalid number has been generated.")
+        {            
+        }
+    }
+}
