8833ac6dbf28f688a444db8d7d5cbaaacadb1103
Loana Albisser <loana.albisser@bbv.ch>
2017-02-16 15:33:20 +0100

    add LoggerFacade

--- KataPatterns/Patterns/Facade/LoggerFacade.cs
+++ KataPatterns/Patterns/Facade/LoggerFacade.cs
@@ -1,17 +1,25 @@
 ﻿using System;
+using log4net;
 
 namespace Patterns.Facade
 {
-    public class Logger
+    public class LoggerFacade
     {
+        private readonly ILog _logger;
+
+        public LoggerFacade()
+        {
+            _logger = LogManager.GetLogger("LoggerFacade");
+        }
+
         public void Log(string message)
         {
-            
+            _logger.Info(message);
         }
 
         public void Log(Exception exception)
         {
-
+            _logger.Info("Exception", exception);
         }
     }
 }
