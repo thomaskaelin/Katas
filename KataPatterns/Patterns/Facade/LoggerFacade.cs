1921600e6f887c69aecd29e036518fd376283331
Loana Albisser <loana.albisser@bbv.ch>
2017-02-16 14:44:19 +0100

    implement Facade

---
+++ KataPatterns/Patterns/Facade/LoggerFacade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Patterns.Facade
+{
+    public class Logger
+    {
+        public void Log(string message)
+        {
+            
+        }
+
+        public void Log(Exception exception)
+        {
+
+        }
+    }
+}
