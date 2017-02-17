b9b09ebe95cc87f9f36c6d492a900978854bc237
Loana Albisser <loana.albisser@bbv.ch>
2017-02-17 09:42:00 +0100

    add unit-tests for composite

---
+++ KataPatterns/Patterns.Test/Composite/MultCompositeFixture.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using FakeItEasy;
+using FluentAssertions;
+using NUnit.Framework;
+using Patterns.Composite;
+
+namespace Patterns.Test.Composite
+{
+    [TestFixture]
+    public class MultCompositeFixture
+    {
+        private MultComposite _testee;
+        private List<IMathOperation> _mathOperationList;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mathOperationList = new List<IMathOperation>();
+            _testee = new MultComposite(_mathOperationList);
+        }
+
+        [Test]
+        public void Calculate_ReturnsExpectedResult()
+        {
+            // Arrange
+            var fakeNumber10 = A.Fake<IMathOperation>();
+            A.CallTo(() => fakeNumber10.Calculate()).Returns(10);
+
+            var fakeNumber20 = A.Fake<IMathOperation>();
+            A.CallTo(() => fakeNumber20.Calculate()).Returns(20);
+
+            _mathOperationList.Add(fakeNumber10);
+            _mathOperationList.Add(fakeNumber20);
+
+            // Act
+            var result = _testee.Calculate();
+
+            // Assert
+            result.Should().Be(200);
+        }
+    }
+}
---
+++ KataPatterns/Patterns.Test/Composite/NumberFixture.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Patterns.Composite;
+
+namespace Patterns.Test.Composite
+{
+    [TestFixture]
+    public class NumberFixture
+    {
+        private Number _testee;
+
+        [TestCase(0,0)]
+        [TestCase(10,10)]
+        [TestCase(100,100)]
+        public void Calculate_ReturnsCorrectNumber(int number, int expectedNumber)
+        {
+            // Arrange
+            _testee = new Number(number);
+
+            // Act
+            var result = _testee.Calculate();
+
+            // Assert
+            result.Should().Be(expectedNumber);
+        }
+    }
+}
