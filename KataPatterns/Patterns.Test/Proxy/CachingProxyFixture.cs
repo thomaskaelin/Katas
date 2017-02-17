4511d345137901797803d5ce8af9fecdd3958680
Loana Albisser <loana.albisser@bbv.ch>
2017-02-17 11:05:14 +0100

    add unit test for proxy

---
+++ KataPatterns/Patterns.Test/Proxy/CachingProxyFixture.cs
@@ -0,0 +1,32 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+using Patterns.Proxy;
+
+namespace Patterns.Test.Proxy
+{
+    [TestFixture]
+    public class CachingProxyFixture
+    {
+        private CachingProxy _testee;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var fakeCalculator = A.Fake<ICalculator>();
+            A.CallTo(() => fakeCalculator.Calculate()).Returns(1);
+            _testee = new CachingProxy(fakeCalculator);
+        }
+
+        [Test]
+        public void Calculate_ReturnsExpectedResult()
+        {
+            // Act
+            var result = _testee.Calculate();
+
+            // Assert
+            result.Should().Be(1);
+        }
+    }
+}
