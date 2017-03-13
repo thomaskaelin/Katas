07697aaff774e28efcf23661047794dadb51de20
Loana Albisser <loana.albisser@bbv.ch>
2017-03-13 10:09:25 +0100

    add Observer Fixture

---
+++ KataPatterns/Patterns.Test/Observer/ModelFixture.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Patterns.Observer;
+
+namespace Patterns.Test.Observer
+{
+    [TestFixture]
+    public class ModelFixture
+    {
+        private Model _testee;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testee = new Model();
+        }
+
+        [Test]
+        public void Refresh_RefreshsValue()
+        {
+            // Act
+            _testee.Refresh();
+
+            // Assert
+            _testee.Value.Should().Be("NewValue");
+        }
+    }
+}
---
+++ KataPatterns/Patterns.Test/Observer/ObserverFixture.cs
@@ -0,0 +1,26 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using NUnit.Framework;
+using Patterns.Observer;
+
+namespace Patterns.Test.Observer
+{
+    [TestFixture]
+    public class ObserverFixture
+    {
+        private Patterns.Observer.Observer _testee;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testee = new Patterns.Observer.Observer();
+        }
+
+        [Test]
+        public void Update_UpdatesValue()
+        {
+            // Act
+            _testee.Update();
+        }
+    }
+}
