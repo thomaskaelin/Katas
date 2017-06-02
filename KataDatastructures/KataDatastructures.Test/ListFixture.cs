86649cf1d9e26f6424f43d60e8fc20ac58f25f75
Thomas Kaelin <thomas.kaelin@bbv.ch>
2017-06-02 15:29:04 +0200

    Data Structures: Started List.

---
+++ KataDatastructures/KataDatastructures.Test/ListFixture.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace KataDatastructures.Test
+{
+    [TestFixture]
+    public class ListFixture
+    {
+        private List<string> _testee;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testee = new List<string>();
+        }
+
+        [Test]
+        public void Add_AddsElementAtTheEndOfTheList()
+        {
+            // Act
+            _testee.Add("1");
+
+            // Assert
+            _testee.Get(0).Should().Be("1");
+        }
+    }
+}
---
+++ KataDatastructures/KataDatastructures/List.cs
@@ -0,0 +1,22 @@
+﻿namespace KataDatastructures
+{
+    public class List<TItem>
+    {
+        private TItem[] _items;
+
+        public List()
+        {
+            _items = new TItem[1];
+        }
+
+        public void Add(TItem item)
+        {
+            _items[0] = item;
+        }
+
+        public TItem Get(int index)
+        {
+            return _items[0];
+        }
+    }
+}
