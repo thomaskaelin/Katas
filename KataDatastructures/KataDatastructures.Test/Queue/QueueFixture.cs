a9e559268d111b0f9575be6aa5e0a353ee2ef743
Loana Albisser <loana.albisser@bbv.ch>
2017-08-11 13:45:18 +0200

    impelement queue

---
+++ KataDatastructures/KataDatastructures.Test/Queue/QueueFixture.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace KataDatastructures.Test.Queue
+{
+    public class QueueFixture
+    {
+        private KataDatastructures.Queue.Queue<string> _testee;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testee = new KataDatastructures.Queue.Queue<string>();
+        }
+
+        [Test]
+        public void Enqueue_AddsItemAtEndOfQueue()
+        {
+            // Arrange
+            _testee.Enqueue("1");
+            _testee.Enqueue("2");
+
+            // Act
+            _testee.Enqueue("3");
+
+            // Assert
+
+            _testee.Dequeue();
+            _testee.Dequeue();
+            
+            var result = _testee.Dequeue();
+            
+            result.Should().Be("3");
+        }
+    }
+}
---
+++ KataDatastructures/KataDatastructures/Queue/Queue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace KataDatastructures.Queue
+{
+    public class Queue <TItem>
+    {
+        private List<TItem> _queue;
+
+        public Queue()
+        {
+            _queue = new List<TItem>();
+        }
+        
+        public void Enqueue(TItem item)
+        {
+            _queue.Add(item);
+        }
+
+        public TItem Dequeue()
+        {
+            var item = _queue[0];
+            _queue.RemoveAt(0);
+            return item;
+        }
+    }
+}
