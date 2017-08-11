625d690f5785cf388c0aeb3561595b827e2f67e1
Loana Albisser <loana.albisser@bbv.ch>
2017-08-11 15:24:57 +0200

    impelement Set

---
+++ KataDatastructures/KataDatastructures.Test/Set/SetFixture.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using KataDatastructures.Set;
+using NUnit.Framework;
+
+namespace KataDatastructures.Test.Set
+{
+    public class SetFixture
+    {
+        private Set<string> _testee;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testee = new Set<string>();
+        }
+
+        [Test]
+        public void Add_WithNewElement_ReturnsTrue()
+        {
+            // Act
+            var result = _testee.Add("1");
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void Add_WithExistingElement_ReturnsFalse()
+        {
+            // Arrange
+            _testee.Add("1");
+
+            // Act
+            var result = _testee.Add("1");
+
+            // Assert
+            result.Should().BeFalse();
+        }
+
+        [Test]
+        public void Contains_WithExistingElement_ReturnsTrue()
+        {
+            // Arrange
+            _testee.Add("1");
+
+            // Act
+            var result = _testee.Contains("1");
+
+            // Assert
+            result.Should().BeTrue();
+        }
+
+        [Test]
+        public void Contains_WithoutExistingElement_ReturnsFalse()
+        {
+            // Act
+            var result = _testee.Contains("1");
+
+            // Assert
+            result.Should().BeFalse();
+        }
+    }
+}
---
+++ KataDatastructures/KataDatastructures/Set/Set.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace KataDatastructures.Set
+{
+    public class Set <TItem>
+    {
+        private Dictionary<TItem, object> _dictionary;
+
+        public Set()
+        {
+            _dictionary = new Dictionary<TItem, object>();
+        }
+
+        public bool Add(TItem item)
+        {
+            if (!Contains(item))
+            {
+                _dictionary.Add(item, null);
+                return true;
+            }
+            return false;
+        }
+
+        public bool Contains(TItem item)
+        {
+            return _dictionary.ContainsKey(item);
+        }
+    }
+}
