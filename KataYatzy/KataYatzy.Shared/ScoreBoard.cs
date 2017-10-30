14633bd7b4faf128d5461b456ba5f0f449c0a510
Thomas Kaelin <thomas.kaelin@bbv.ch>
2017-10-30 16:42:07 +0100

    Refactoring: added TODO's for validation.

--- KataYatzy/KataYatzy.Shared/ScoreBoard.cs
+++ KataYatzy/KataYatzy.Shared/ScoreBoard.cs
@@ -23,19 +23,23 @@ namespace KataYatzy.Shared
 
         public void AddPlayer(IPlayer player)
         {
+            // TODO Validierung: Player null?
+            // TODO Validierung: Player schon vorhanden?
             Player.Add(player);
         }
 
         public void AddCombination(ICombination combination)
         {
+            // TODO Validierung: Combination null?
+            // TODO Validierung: Combination schon vorhanden?
             Combinations.Add(combination);
         }
 
         public void AssignToss(IPlayer player, IToss toss, CombinationType combinationType)
         {
-            // TODO Was passiert wenn kein Player
-            // TODO Was passiert wenn CombinationType nicht exisitert
-            // TODO Was passiert wenn für CombinationType schon ein Toss exisitiert
+            // TODO Validierung: Was passiert, wenn Player nicht vorhanden ist?
+            // TODO Validierung: Was passiert, wenn keine Combination für den CombinationType registriert ist?
+            // TODO Validierung: Was passiert, wenn für den CombinationType schon ein Toss zugewiesen wurde?
             var newTossMapping = new TossMapping
             {
                 Player = player,
@@ -48,7 +52,9 @@ namespace KataYatzy.Shared
 
         public IPoints GetPointsForCombination(IPlayer player, CombinationType combinationType)
         {
-            // TODO Was passiert wenn CombinationType nicht exisitert
+            // TODO Validierung: Was passiert, wenn Player nicht vorhanden ist?
+            // TODO Validierung: Was passiert, wenn keine Combination für den CombinationType registriert ist?
+            // TODO Validierung: Was passiert, wenn keine Punkte für den CombinationType hinzugefügt wurden?
             var matchingMapping = _tossMappings.First(mapping => mapping.Player == player && mapping.CombinationType == combinationType);
             var matchingCombination = Combinations.First(combination => combination.Type == combinationType);
             var pointsForCombination = matchingCombination.Calculate(matchingMapping.Toss);
@@ -58,6 +64,7 @@ namespace KataYatzy.Shared
 
         public IPoints GetTotalPoints(IPlayer player)
         {
+            // TODO Validierung: Was passiert, wenn Player nicht vorhanden ist?
             var matchingMappings = _tossMappings.FindAll(mapping => mapping.Player == player);
             var totalPointsAsInt = matchingMappings.Sum(matchingMapping => GetPointsForCombination(player, matchingMapping.CombinationType).Value);
             var totalPointsAsPoints = new Points(totalPointsAsInt);
