9ae150058c16b2f2741d7bf2b3ec1f20f57d541b
Thomas Kaelin <thomas.kaelin@bbv.ch>
2018-02-02 14:42:44 +0100

    Yatzy: added UI.WPF with a starter for a WPF application.

---
+++ KataYatzy/KataYatzy.UI.WPF/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using KataYatzy.UI.VM;
+
+namespace KataYatzy.UI.WPF
+{
+    public partial class MainWindow : Window
+    {
+        private readonly ScoreBoardViewModel _scoreBoardViewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _scoreBoardViewModel = new ScoreBoardViewModel();
+            DataContext = _scoreBoardViewModel;
+        }
+    }
+}
