ab2d404df0dde4a9000a05fb8ae62845c01985f9
Thomas Kälin <thomaskaelin@bbv.ch>
2018-02-07 13:58:57 +0100

    Yatzy: added unit tests for Points.

---
+++ KataYatzy/KataYatzy.Shared.Test/PointsFixture.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace KataYatzy.Shared.Test
+{
+    [TestFixture]
+    public class PointsFixture
+    {
+        private Points _testee;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testee = new Points(321);
+        }
+
+        [Test]
+        public void Constructor_InitializesValue()
+        {
+            // Assert
+            _testee.Value.Should().Be(321);
+        }
+    }
+}
