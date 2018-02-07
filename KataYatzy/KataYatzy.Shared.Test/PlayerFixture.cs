9289ccaad4e916328f2bccbeedcfd65d67c937ba
Thomas Kälin <thomaskaelin@bbv.ch>
2018-02-07 14:05:54 +0100

    Yatzy: added unit tests for Player.

---
+++ KataYatzy/KataYatzy.Shared.Test/PlayerFixture.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace KataYatzy.Shared.Test
+{
+    [TestFixture]
+    public class PlayerFixture
+    {
+        private Player _testee;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testee = new Player("Loana");
+        }
+
+        [Test]
+        public void Constructor_InitializesName()
+        {
+            // Assert
+            _testee.Name.Should().Be("Loana");
+        }
+    }
+}
